3b6d2b5332496f942589385389200fc72d3ac9d2
madhura <madhuranikam089@gmail.com>
2025-07-08 16:16:13 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import './styles/pages.css';
+import './styles/Pages.css';
 import AboutPage from "./pages/AboutPage";
 import CoursesPage from "./pages/CoursesPage";
 import AdmissionsPage from "./pages/AdmissionsPage";
